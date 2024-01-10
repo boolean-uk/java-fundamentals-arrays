eaf97b10740948262af3823463beee13706eb6be
Petros Mylonas <petrosmylonascs@gmail.com>
2024-01-10 15:58:29 +0200

    Petros Mylonas

--- src/main/java/com/booleanuk/core/Exercise.java
+++ src/main/java/com/booleanuk/core/Exercise.java
@@ -24,7 +24,7 @@ public class Exercise {
         // WRITE YOUR CODE BETWEEN THIS LINE...
 
 
-
+        numbers[2]=68;
         // ... AND THIS LINE
 
         return numbers;
@@ -37,7 +37,7 @@ public class Exercise {
         // teacher contained in the teachers array
         // WRITE YOUR CODE BETWEEN THIS LINE...
 
-        String teacher = "";
+        String teacher = teachers[3];
 
         // ... AND THIS LINE
 
@@ -47,13 +47,13 @@ public class Exercise {
     public String[] three() {
         // 3. Create a string array named cars that contains three names of car manufacturers: Audi, BMW and Dodge
         // WRITE YOUR CODE BETWEEN THIS LINE...
-
+        String[] cars={"Audi","BMW","Dodge"};
 
 
         // ... AND THIS LINE
 
         // Then change the code below to remove the dummyArray completely and return the cars array you created above instead.
-        String[] dummyArray = {};
+        String[] dummyArray = cars;
         return dummyArray;
     }
 
@@ -65,7 +65,9 @@ public class Exercise {
         // WRITE YOUR CODE BETWEEN THIS LINE...
 
         int result = 0;
-
+        int i ;
+        for (i = 0; i < numbers.length; i++)
+            result += numbers[i];
         // ... AND THIS LINE
 
         return result;
@@ -74,13 +76,13 @@ public class Exercise {
     public float[] five() {
         // 5. Create an array called floats that contains 3 floating point numbers: 9.62, 23.17 and 3.14
         // WRITE YOUR CODE BETWEEN THIS LINE...
-
+        float[] floats={9.62f,23.17f,3.14f};
 
 
         // ... AND THIS LINE
 
         // Then change the code below to remove the dummyArray completely and return the floats array you created instead
-        float[] dummyArray = {};
+        float[] dummyArray = floats;
         return dummyArray;
     }
 }
