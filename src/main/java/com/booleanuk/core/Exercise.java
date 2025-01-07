07db59bf0d88cde1ced886a37f95347d814a40a6
Jonas Finborud Nyman <jonas.nyman@no.experis.com>
2025-01-07 15:53:29 +0100

    Finished exercises

--- src/main/java/com/booleanuk/core/Exercise.java
+++ src/main/java/com/booleanuk/core/Exercise.java
@@ -22,7 +22,7 @@ public class Exercise {
         // E.g. The first value is at index 0, the second at index 1, the third at index 2.
         // Using an index, change the number 17 in the numbers array to 68
         // WRITE YOUR CODE BETWEEN THIS LINE...
-
+        numbers[2] = 68;
 
 
         // ... AND THIS LINE
@@ -38,6 +38,7 @@ public class Exercise {
         // WRITE YOUR CODE BETWEEN THIS LINE...
 
         String teacher = "";
+        teacher = teachers[3];
 
         // ... AND THIS LINE
 
@@ -47,14 +48,13 @@ public class Exercise {
     public String[] three() {
         // 3. Create a string array named cars that contains three names of car manufacturers: Audi, BMW and Dodge
         // WRITE YOUR CODE BETWEEN THIS LINE...
-
+        String[] cars = {"Audi", "BMW", "Dodge"};
 
 
         // ... AND THIS LINE
 
         // Then change the code below to remove the dummyArray completely and return the cars array you created above instead.
-        String[] dummyArray = {};
-        return dummyArray;
+        return cars;
     }
 
     public int four() {
@@ -65,6 +65,9 @@ public class Exercise {
         // WRITE YOUR CODE BETWEEN THIS LINE...
 
         int result = 0;
+        for(int i : numbers) {
+            result += i;
+        }
 
         // ... AND THIS LINE
 
@@ -74,13 +77,12 @@ public class Exercise {
     public float[] five() {
         // 5. Create an array called floats that contains 3 floating point numbers: 9.62, 23.17 and 3.14
         // WRITE YOUR CODE BETWEEN THIS LINE...
-
+        float[] floats = {9.62F, 23.17F, 3.14F};
 
 
         // ... AND THIS LINE
 
         // Then change the code below to remove the dummyArray completely and return the floats array you created instead
-        float[] dummyArray = {};
-        return dummyArray;
+        return floats;
     }
 }
